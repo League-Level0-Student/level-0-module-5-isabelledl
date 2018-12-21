d1a9e1adace0bd356c01898bef90cd5808c5e99c
isabelledl <league@WTS-IM-17.attlocal.net>
2018-12-20 20:00:13 -0800

    loop(IP)

---
+++ src/loop.java
@@ -0,0 +1,25 @@
+
+public class loop {
+public static void main(String[] args) {
+	
+		for (int i = 0; i < 101; i++) {
+			System.out.println(i);
+
+		}
+		for (int i = 0; i < 101; i++) {
+		System.out.println( i );
+			
+		}
+		for (int i = 2; i <100 ; i++) {
+			System.out.println(i);
+		}
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
