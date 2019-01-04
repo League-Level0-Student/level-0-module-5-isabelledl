99a9aaf166be245341ba6a4cbe23e0ed3c61db9e
isabelledl <league@WTS22.attlocal.net>
2019-01-03 19:58:44 -0800

    loop(IP)

--- src/loop.java
+++ src/loop.java
@@ -1,25 +1,30 @@
 
 public class loop {
-public static void main(String[] args) {
-	
+	public static void main(String[] args) {
+
 		for (int i = 0; i < 101; i++) {
 			System.out.println(i);
 
 		}
 		for (int i = 0; i < 101; i++) {
-		System.out.println( i );
-			
+			System.out.println(i);
+
 		}
-		for (int i = 2; i <100 ; i++) {
+		for (int i = 2; i < 101; i += 2) {
 			System.out.println(i);
 		}
-	
-	
-	
-	
-	
-	
-	
-	
-}
+		for (int i = 1; i < 100; i--) {
+
+		}
+		for (int i = 0; i < 501; i++) {
+
+			if (i % 2 == 0) {
+				System.out.println(i + " is even");
+			} else {
+				System.out.println(i + " is odd ");
+
+			}
+			System.out.println();
+		}
+	}
 }
